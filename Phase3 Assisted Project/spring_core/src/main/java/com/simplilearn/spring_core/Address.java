352ad55751b63954331069ca1ce26fa4512c4299
Pragati198 <pragatimohite1611@gmail.com>
2022-03-24 17:50:04 +0530

    Adding

---
+++ Phase3 Assisted Project/spring_core/src/main/java/com/simplilearn/spring_core/Address.java
@@ -0,0 +1,26 @@
+package com.simplilearn.spring_core;
+
+
+import java.util.List;
+
+public class Address {
+	
+	
+	private  List<String> cities;
+	
+	public Address()
+	{
+		
+	}
+	public List<String> getCities() {
+		return cities;
+	}
+
+	public void setCities(List<String> cities) {
+		this.cities = cities;
+	}
+	
+	
+	
+
+}
